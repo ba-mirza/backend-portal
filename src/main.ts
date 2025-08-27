2e1e29fd54683a5b627523b93f95fab98f56dad8
dev <almas1610@gmail.com>
2025-08-27 18:59:59 +0500

    [deploy]: get-ready for deploy to DO

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import * as process from 'node:process';
+
+const logger = new Logger();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = ['http://localhost:5173'];
+  if (process.env.NODE_ENV === 'production') {
+    allowedOrigins.push('vercel.app');
+  }
+
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: allowedOrigins,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
   });
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  await app.listen(process.env.PORT ?? 3002);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableShutdownHooks();
+
+  const PORT = process.env.PORT || 3002;
+
+  await app.listen(PORT, '0.0.0.0');
+
+  logger.log(`🚀 Server running on port ${PORT}`);
+  logger.log(`📖 API available at: http://localhost:${PORT}/api`);
 }
 
 bootstrap();
